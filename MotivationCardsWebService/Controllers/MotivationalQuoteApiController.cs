66f81d9a6aa6529e63a948b890651dce54e9900c
Mate Granić <mate.granic@rimac-technology.com>
2023-09-02 10:26:49 +0200

    Added GET method to web service to get motivational quote

---
+++ MotivationCardsWebService/Controllers/MotivationalQuoteApiController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MotivationCardsWebService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MotivationalQuoteApiController : ControllerBase
+{
+
+   // https://localhost:7081/MotivationalQuoteApi?name=GetWeatherForecast
+   // [HttpGet(Name = "GetWeatherForecast")]
+   //localhost:5079/MotivationalQuoteApi/GetMotivationalQuote/radiiiii
+   [HttpGet("GetMotivationalQuote/{param}")]
+    public String GetMotivationalQuote(String param)
+    {
+        Console.WriteLine(param);
+       //return "{\"type\": \"json object\"}";
+       return param;
+    }
+}
