624c64feed6cccbbababa402cdbb2fe852198124
mgranic <mgranic11@gmail.com>
2023-09-20 17:58:04 +0200

    Personal data view
    started creating view where you can edit your personal data. Also adding model for that

---
+++ MotivationCards/MotivationCards/Model/PersonalDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalDataModel.swift
+//  MotivationCards
+//
+//  Created by Mate Granic on 19.09.2023..
+//
+
+import Foundation
+
+enum SexE: String {
+    case none
+    case male
+    case female
+}
+
+enum MotivationE: String {
+    case none
+    case uplift
+    case console
+    case motivation
+}
